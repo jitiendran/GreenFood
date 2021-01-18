143123887e654d91161764bf3e41e51c52dc84df
jitiendran <jitiendranjiji2000@gmail.com>
2021-01-18 21:01:02 +0530

    A simiple angular food website

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { transition } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  click: boolean = false;
+  constructor() { }
+  ngOnInit(): void {
+  }
+}
